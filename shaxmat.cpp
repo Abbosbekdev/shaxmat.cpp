c8755b030257cecc533f4b426e5df5581d09456f
Abbosbek Ustaboyev Botirjonovich <91521657+Abbosbekdev@users.noreply.github.com>
2021-10-07 10:25:59 +0500

    Create shaxmat.cpp
    c++ dasturlash tilidan shaxmat doskasini chiqarish

---
+++ shaxmat.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define black cout<<char(219)<<char(219);
+#define white cout<<char(179)<<char(179);
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+	for(int j=0;j<n;j++){
+	if((i+j)%2==0) black
+	else white
+}cout<<endl;
+	}		
+}
